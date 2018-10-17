e9fb9b18aee21b79925806ce86cb3963c017f6de
mclancerlot <lance@RCTID.local>
2018-10-16 20:08:53 -0700

    Steps 13-24

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Business.css';
 
 const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
@@ -15,24 +16,24 @@ const business = {
 class Business extends React.Component {
   render() {
     return (
-      <div class="Business">
-        <div class="image-container">
+      <div className="Business">
+        <div className="image-container">
           <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
         </div>
-        <h2>MarginOtto Pizzeria</h2>
-        <div class="Business-information">
-          <div class="Business-address">
-            <p>1010 Paddington Way</p>
-            <p>Bordertown</p>
-            <p>NY 10101</p>
+        <h2>{this.name}</h2>
+        <div className="Business-information">
+          <div className="Business-address">
+            <p>{this.address}</p>
+            <p>{this.city}</p>
+            <p>{this.state} {this.zipCode}</p>
           </div>
-          <div class="Business-reviews">
-            <h3>ITALIAN</h3>
-            <h3 class="rating">4.5 stars}</h3>
-            <p>90 reviews</p>
+          <div className="Business-reviews">
+            <h3>{this.category.toUpperCase()}</h3>
+            <h3 className="rating">{this.rating} stars}</h3>
+            <p>{this.reviewCount} reviews</p>
           </div>
         </div>
       </div>
     );
   }
-}
+export default Business;
